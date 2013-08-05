ddcf8f8e36a451261c621b342a28aa4ca9fe8f53
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-08-05 23:39:05 +0200

    Raised version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from glob import glob
 import sys
 
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 if sys.platform == 'win32' and '32 bit' in sys.version:
     extra_objects = ['src/switch_x86_msvc.obj']
