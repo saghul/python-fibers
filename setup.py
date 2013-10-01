50c5672c88ca34e9543ec20e445fa177209aeaf1
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-10-02 00:48:28 +0200

    Added PyPy classifier

--- setup.py
+++ setup.py
@@ -52,7 +52,9 @@ setup(name             = "fibers",
           "Programming Language :: Python :: 3.0",
           "Programming Language :: Python :: 3.1",
           "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3"
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
       ],
       ext_modules  = ext_modules
      )
