634378450ce35dcfc372777d35d36bb7c940afd9
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-07-21 22:11:06 +0200

    Refactor Windows build
    Don't bundle assembled files, do it at build time.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, Extension
 
 import glob
+import os
 import re
 import sys
 
@@ -10,12 +11,24 @@ import sys
 if hasattr(sys, 'pypy_version_info'):
     ext_modules = []
 else:
-    if sys.platform == 'win32' and '32 bit' in sys.version:
-        extra_objects = ['src/switch_x86_msvc.obj']
-    elif sys.platform == 'win32' and '64 bit' in sys.version:
-        extra_objects = ['src/switch_x64_msvc.obj']
-    else:
-        extra_objects = []
+    extra_objects = []
+    if sys.platform == 'win32':
+        # This is a hack because msvc9compiler doesn't support asm files
+        # http://bugs.python.org/issue7546
+
+        # Initialize compiler
+        from distutils.msvc9compiler import MSVCCompiler
+        cc = MSVCCompiler()
+        cc.initialize()
+        del cc
+
+        if '32 bit' in sys.version:
+            extra_objects = ['src/switch_x86_msvc.obj']
+            os.system('ml /nologo /c /Fo src\switch_x86_msvc.obj src\switch_x86_msvc.asm')
+        else:
+            extra_objects = ['src/switch_x64_msvc.obj']
+            os.system('ml64 /nologo /c /Fo src\switch_x64_msvc.obj src\switch_x64_msvc.asm')
+
     ext_modules  = [Extension('fibers._cfibers',
                               sources = glob.glob('src/*.c'),
                               extra_objects=extra_objects,
